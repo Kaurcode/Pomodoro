8bf024c296c16b53ca16c341176bb31c3be75352
Kaurcode <kaurkivi@gmail.com>
2024-03-24 19:49:06 +0200

    Lisatud sisend

--- src/Taimer.java
+++ src/Taimer.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Taimer {
     private int aegaJarel;
     private int esialgneAeg;
+    Scanner loeKlaviatuurilt = new Scanner(System.in);
     private Timer taimer = new Timer();
 
     TimerTask loendaja = new TimerTask() {
@@ -21,21 +23,26 @@ public class Taimer {
     }
 
     public void alustaLoendust() {
-        System.out.print("\033[H\033[2J");  // Kustutab ekraani
-        System.out.flush();
+        System.out.print("\033[1;1H");  // Paneb kursori ülemisse vasakusse nurka
+        System.out.println("\033[0J");  // Kustuta kõik alates kursorist
+        System.out.print("\033[2E");  // Aseta kursor kaks rida edasi vasakusse äärde
+        System.out.print("Sisend: ");
         taimer.scheduleAtFixedRate(loendaja, 0, 1000);  // Alustab loeandust
+        while (!loeKlaviatuurilt.hasNextLine());  // Väga halb variant, tuleks midagi paremat välja mõelda
+        taimer.cancel();  // Lõpetab loenduse
     }
 
     public void valjastaAeg(int aegSekundites) {
-        System.out.print("\b".repeat(54));  // Et IDE-s näeks parem välja, konsoolis pole vaja kasutada
-        System.out.print("\033[H");  // https://www.javatpoint.com/how-to-clear-screen-in-java -> Töötab ainult konsoolis
-
+        System.out.print("\033[s");  // Salvestab kursori positsiooni (Et saaks hiljem konsooli kirjutada)
         // Kui loendab, aega, siis on ees miinus märk ("-"), kui on üle aja läinud, siis on ees pluss märk ("+")
         char mark = switch(Integer.signum(aegSekundites)) {
             case 1 -> '-';
             case -1 -> '+';
             default -> ' ';
         };
+
+        System.out.print("\033[2F");  // Kursor läheb kaks rida üles ja vasakusse äärde
+        System.out.print("\r");  // Kustututab reas (et näeks IDE-s parem välja, konsoolis pole vaja)
 
         // Näitsab protsentribal, kui palju aega eesmärgist on kulunud
         int protsent = Math.max(0, Math.round(100 * ((float) aegSekundites / esialgneAeg)));
@@ -49,5 +56,7 @@ public class Taimer {
         // Näitab aega protsentriba all, joondatud keskele
         int minutiMarke = Math.max(2, String.valueOf(minuteid).length());
         System.out.printf("%s%c%02d:%02d", " ".repeat(50 - minutiMarke), mark, minuteid, sekundeid);
+
+        System.out.print("\033[u");  // Taastab kursori salvestatud positsiooni, läheb algsesse kohta tagasi
     }
 }
