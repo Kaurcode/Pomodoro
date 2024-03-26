103d168e0e6659d6609036d1cd14d95b39321bcf
Kaurcode <kaurkivi@gmail.com>
2024-03-26 17:22:53 +0200

    Progress baril on nüüd hele ots + kood veits parem

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\033[");
         CLIElemendid.looRGB();
         Scanner klaviatuuriSisend = new Scanner(System.in);
 
